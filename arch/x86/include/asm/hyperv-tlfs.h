8a8b6e66aea3092b73c6dfd6828d7c443b020bd4
Ryan Fairfax <rfairfax@microsoft.com>
2022-10-24 08:18:58 -0700

    x86/hyperv:  Move common data structures outside of arch/x86
    Create new hyperv-common-types header and adjust callers to use it for
    non-x86 specific data structures
    
    Signed-off-by: Ryan Fairfax <rfairfax@microsoft.com>

--- arch/x86/include/asm/hyperv-tlfs.h
+++ arch/x86/include/asm/hyperv-tlfs.h
@@ -11,6 +11,7 @@
 
 #include <linux/types.h>
 #include <asm/page.h>
+#include <asm-generic/hyperv-common-types.h>
 #include <uapi/asm/hyperv-tlfs.h>
 /*
  * The below CPUID leaves are present if VersionAndFeatures.HypervisorPresent
