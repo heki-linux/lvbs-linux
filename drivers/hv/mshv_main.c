a3aee25ca4e26d49793193902190a53af386b337
Nuno Das Neves <nunodasneves@linux.microsoft.com>
2022-12-10 01:13:38 +0000

    mshv: Add new file mshv_common.c
    Contains common functions for mshv modules
    
    Signed-off-by: Nuno Das Neves <nunodasneves@linux.microsoft.com>

--- drivers/hv/mshv_main.c
+++ drivers/hv/mshv_main.c
@@ -656,38 +656,6 @@ mshv_vp_ioctl_get_set_state(struct mshv_vp *vp, void __user *user_args, bool is_
 
 #endif
 
-long
-mshv_ioctl_translate_gva(u32 vp_index, u64 partition_id, void __user *user_args)
-{
-	long ret;
-	struct mshv_translate_gva args;
-	u64 gpa;
-	union hv_translate_gva_result result;
-
-	if (copy_from_user(&args, user_args, sizeof(args)))
-		return -EFAULT;
-
-	ret = hv_call_translate_virtual_address(
-			vp_index,
-			partition_id,
-			args.flags,
-			args.gva,
-			&gpa,
-			&result);
-
-	if (ret)
-		return ret;
-
-	if (copy_to_user(args.result, &result, sizeof(*args.result)))
-		return -EFAULT;
-
-	if (copy_to_user(args.gpa, &gpa, sizeof(*args.gpa)))
-		return -EFAULT;
-
-	return 0;
-}
-EXPORT_SYMBOL_GPL(mshv_ioctl_translate_gva);
-
 #ifdef HV_SUPPORTS_REGISTER_INTERCEPT
 
 static long
@@ -1137,38 +1105,6 @@ mshv_partition_ioctl_unmap_memory(struct mshv_partition *partition,
 	return 0;
 }
 
-long
-mshv_ioctl_install_intercept(u64 partition_id, void __user *user_args)
-{
-	struct mshv_install_intercept args;
-
-	if (copy_from_user(&args, user_args, sizeof(args)))
-		return -EFAULT;
-
-	return hv_call_install_intercept(
-			partition_id,
-			args.access_type_mask,
-			args.intercept_type,
-			args.intercept_parameter);
-}
-EXPORT_SYMBOL_GPL(mshv_ioctl_install_intercept);
-
-long
-mshv_ioctl_assert_interrupt(u64 partition_id, void __user *user_args)
-{
-	struct mshv_assert_interrupt args;
-
-	if (copy_from_user(&args, user_args, sizeof(args)))
-		return -EFAULT;
-
-	return hv_call_assert_virtual_interrupt(
-			partition_id,
-			args.vector,
-			args.dest_addr,
-			args.control);
-}
-EXPORT_SYMBOL_GPL(mshv_ioctl_assert_interrupt);
-
 static long
 mshv_partition_ioctl_ioeventfd(struct mshv_partition *partition,
 		void __user *user_args)
@@ -1254,56 +1190,6 @@ mshv_partition_ioctl_set_msi_routing(struct mshv_partition *partition,
 
 	return ret;
 }
-
-long
-mshv_ioctl_signal_event_direct(u64 partition_id, void __user *user_args)
-{
-	struct mshv_signal_event_direct args;
-	long ret;
-
-	if (copy_from_user(&args, user_args, sizeof(args)))
-		return -EFAULT;
-
-	ret = hv_call_signal_event_direct(args.vp,
-					partition_id,
-					args.vtl,
-					args.sint,
-					args.flag,
-					&args.newly_signaled);
-
-	if (ret) 
-		return ret;
-	
-	if (copy_to_user(user_args, &args, sizeof(args)))
-		return -EFAULT;
-
-	return 0;
-}
-EXPORT_SYMBOL_GPL(mshv_ioctl_signal_event_direct);
-
-long
-mshv_ioctl_post_message_direct(u64 partition_id, void __user *user_args)
-{
-	struct mshv_post_message_direct args;
-	u8 message[HV_MESSAGE_SIZE];
-
-	if (copy_from_user(&args, user_args, sizeof(args)))
-		return -EFAULT;
-
-	if (args.length > HV_MESSAGE_SIZE) 
-		return -E2BIG;
-
-	memset(&message[0], 0, sizeof(message));
-	if (copy_from_user(&message[0], args.message, args.length))
-		return -EFAULT;
-
-	return hv_call_post_message_direct(args.vp,
-					partition_id,
-					args.vtl,
-					args.sint,
-					&message[0]);
-}
-EXPORT_SYMBOL_GPL(mshv_ioctl_post_message_direct);
 
 #ifdef HV_SUPPORTS_REGISTER_DELIVERABILITY_NOTIFICATIONS
 static long 
