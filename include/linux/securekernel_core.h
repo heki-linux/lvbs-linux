dc9b8a33c4e725f667dab3a6c1d6451371a170de
Anna Trikalinou <atrikalinou@microsoft.com>
2022-12-20 22:07:02 -0800

    Add new securekernel kernel command line argument

---
+++ include/linux/securekernel_core.h
@@ -0,0 +1,12 @@
+
+#ifndef LINUX_SECUREKERNEL_CORE_H
+#define LINUX_SECUREKERNEL_CORE_H
+
+#include <linux/linkage.h>
+#include <linux/elfcore.h>
+#include <linux/elf.h>
+
+int __init parse_securekernel(char *cmdline, unsigned long long system_ram,
+		unsigned long long *securekernel_size, unsigned long long *securekernel_base);
+
+#endif /* LINUX_SECUREKERNEL_CORE_H */
