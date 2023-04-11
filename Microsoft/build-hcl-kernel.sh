b7df4adc7c5313350987dc9204987468034f6416
kromych <kromych@users.noreply.github.com>
2023-04-11 13:23:22 -0700

    Rename the kernel binary and clean our directory

--- Microsoft/build-hcl-kernel.sh
+++ Microsoft/build-hcl-kernel.sh
@@ -9,7 +9,6 @@ O=`getopt -n "$0" -l help -- nh "$@"` || usage
 eval set -- "$O"
 
 builds=()
-build_init=
 desc=()
 clean=1
 
@@ -32,7 +31,6 @@ while true; do
 			echo ""
 			echo "  Available builds:"
 			echo "    dev"
-			echo "    init"
 			echo ""
 			exit
 			;;
@@ -44,7 +42,6 @@ done
 
 if [ $# == 0 ]; then
 	builds=(dev)
-	build_init=1
 	desc=("dev")
 else
 	while [ $# != 0 ]; do
@@ -52,9 +49,6 @@ else
 			dev)
 				builds+=(dev)
 				desc+=("dev")
-				;;
-			init)
-				build_init=1
 				;;
 			*)
 				>&2 echo "Unknown build type: $1"
@@ -74,21 +68,21 @@ build_kernel() {
 	if [ -n "$clean" ]; then
 		make mrproper
 	fi
-	export KCONFIG_CONFIG=$LINUX_SRC/Microsoft/hcl-${BUILD_TYPE}.config
+	export KCONFIG_CONFIG=$LINUX_SRC/Microsoft/hcl.config
 	# For the verbose build
 	#make SHELL='sh -x' ARCH=x86_64 -j `nproc` 2> ${BUILD_DIR}/hcl-build-verbose.log
 	make ARCH=x86_64 -j `nproc` olddefconfig vmlinux modules
-	cp $LINUX_SRC/Microsoft/hcl-${BUILD_TYPE}.config $OUT_DIR
-	objcopy --only-keep-debug --compress-debug-sections $KBUILD_OUTPUT/vmlinux $BUILD_DIR/vmlinux-${BUILD_TYPE}.debug
-	objcopy --strip-all --add-gnu-debuglink=$BUILD_DIR/vmlinux-${BUILD_TYPE}.debug $KBUILD_OUTPUT/vmlinux $BUILD_DIR/vmlinux-${BUILD_TYPE}
+	cp $LINUX_SRC/Microsoft/hcl.config $OUT_DIR
+	objcopy --only-keep-debug --compress-debug-sections $KBUILD_OUTPUT/vmlinux $BUILD_DIR/vmlinux.debug
+	objcopy --strip-all --add-gnu-debuglink=$BUILD_DIR/vmlinux.debug $KBUILD_OUTPUT/vmlinux $BUILD_DIR/vmlinux
 	find $BUILD_DIR -name '*.ko' | while read -r mod; do
 		outmod="$OUT_DIR/$(basename $mod)"
 		objcopy --only-keep-debug --compress-debug-sections "$mod" "$outmod.debug"
 		objcopy --strip-unneeded --add-gnu-debuglink "$outmod.debug" "$mod" "$outmod"
 	done
-	cp $BUILD_DIR/vmlinux-${BUILD_TYPE} $OUT_DIR
-	cp $BUILD_DIR/vmlinux-${BUILD_TYPE}.debug $OUT_DIR
-	cp $LINUX_SRC/Microsoft/hcl-${BUILD_TYPE}.config $OUT_DIR
+	cp $BUILD_DIR/vmlinux $OUT_DIR
+	cp $BUILD_DIR/vmlinux.debug $OUT_DIR
+	cp $LINUX_SRC/Microsoft/hcl.config $OUT_DIR
 }
 
 LINUX_SRC=$SRC_DIR
@@ -96,8 +90,6 @@ BUILD_DIR=`realpath $LINUX_SRC/../build`
 OUT_DIR=`realpath $LINUX_SRC/out`
 
 export KBUILD_OUTPUT=$BUILD_DIR/linux
-export HCL_INIT=${BUILD_DIR}/hcl-init
-export UNDERHILL=/dev/null
 
 if [ -n "$clean" ]; then
 	rm -rf $KBUILD_OUTPUT
@@ -109,7 +101,6 @@ mkdir -p $OUT_DIR
 
 cd $LINUX_SRC
 
-cp $SCRIPT_DIR/HCL-HOWTO.md $OUT_DIR
 cp $SCRIPT_DIR/*.cpio.gz $OUT_DIR
 cp $SCRIPT_DIR/*.config $OUT_DIR
 
@@ -119,19 +110,6 @@ do
 	BUILD_TYPE=${1:-${builds[b]}}
 	build_kernel
 done
-
-if [ -n "$build_init" ]; then
-	echo "Building init"
-
-	INCLUDE_DIR="-I${BUILD_DIR}/include -I${LINUX_SRC}/include -I${LINUX_SRC}/arch/x86/include -I${KBUILD_OUTPUT}/include"
-	klcc ${INCLUDE_DIR} ${SCRIPT_DIR}/hcl-init.c -o ${BUILD_DIR}/hcl-init
-	cp ${BUILD_DIR}/hcl-init ${BUILD_DIR}/hcl-init.unstripped
-
-	execstack -c ${BUILD_DIR}/hcl-init
-	strip ${BUILD_DIR}/hcl-init
-	cp $BUILD_DIR/hcl-init $OUT_DIR
-	cp $BUILD_DIR/hcl-init.unstripped $OUT_DIR
-fi
 
 echo "Installing headers to ${BUILD_DIR}"
 rm -rf $BUILD_DIR/include
