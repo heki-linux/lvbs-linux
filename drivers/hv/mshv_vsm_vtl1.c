ed2d5d3f97aebac6d0d0966d7e6322ee4573e9d7
Thara Gopinath <tgopinath@microsoft.com>
2023-09-25 11:57:24 +0000

    General clean up and fixes in VTL1 driver.
    General clean up and fixes in VTL1 driver.
    
    Signed-off-by: Thara Gopinath <tgopinath@microsoft.com>

--- drivers/hv/mshv_vsm_vtl1.c
+++ drivers/hv/mshv_vsm_vtl1.c
@@ -23,49 +23,15 @@ enum vsm_service_ids {
 	VSM_VTL_CALL_FUNC_ID_PROTECT_MEMORY = 0x1FFF,
 	VSM_VTL_CALL_FUNC_ID_LOCK_CR = 0x1FFFF
 };
-char hv_hypercall_input_page[VSM_PAGE_SIZE] __aligned(VSM_PAGE_SIZE);
-char hv_hypercall_output_page[VSM_PAGE_SIZE] __aligned(VSM_PAGE_SIZE);
-
-static void *hv_acquire_hypercall_input_page(void)
-{
-	memset(hv_hypercall_input_page, 0, sizeof(hv_hypercall_input_page));
-
-	return hv_hypercall_input_page;
-}
-
-enum hv_status vsm_hv_hypercall(u64 call_code, void *input_page,
-	void *output_page, u32 count_of_elements, u32 *elements_processed)
-{
-	union hv_hypercall_input input = { 0 };
-	union hv_hypercall_output output = { 0 };
-
-	input.call_code = call_code;
-	input.count_of_elements = count_of_elements;
-
-	output.as_uint64 = hv_do_hypercall(input.as_uint64, input_page,
-		output_page);
-
-	if (elements_processed)
-		*elements_processed = output.elements_processed;
-
-	return output.call_status;
-}
 
 static int hv_modify_vtl_protection_mask(u64 gpa_page_list[],
 	size_t *number_of_pages, u32 page_access)
 {
-	enum hv_status status;
-	u64 flags;
-	int64_t pages_remaining;
-	u32 pages_processed;
-	u32 total_pages_processed;
-	size_t max_pages_per_request;
-	u32 i;
-	union hv_input_vtl target_vtl;
-
-	target_vtl.as_uint8 = 1;
-
 	struct hv_input_modify_vtl_protection_mask *hvin;
+	u64 status, pages_processed, total_pages_processed;
+	unsigned long flags;
+	size_t max_pages_per_request;
+	int i;
 
 	/* Check parameters */
 	if (!gpa_page_list || !number_of_pages || *number_of_pages >= UINT_MAX)
@@ -74,12 +40,15 @@ static int hv_modify_vtl_protection_mask(u64 gpa_page_list[],
 	/* Compute the maximum number of pages that can be processed in one go */
 	max_pages_per_request = (VSM_PAGE_SIZE - sizeof(*hvin)) / sizeof(u64);
 
+	/* Disable interrupts */
+	local_irq_save(flags);
+
 	/* Acquire the input page */
-	hvin = hv_acquire_hypercall_input_page();
+	hvin = (struct hv_input_modify_vtl_protection_mask *)(*this_cpu_ptr(hyperv_pcpu_input_arg));
 
 	/* Fill in the hypercall parameters */
 	hvin->partition_id = HV_PARTITION_ID_SELF;
-	hvin->target_vtl = target_vtl;
+	hvin->target_vtl.as_uint8 = 1;
 	hvin->map_flags = page_access;
 
 	/*
@@ -88,51 +57,41 @@ static int hv_modify_vtl_protection_mask(u64 gpa_page_list[],
 	 */
 	pages_processed = 0;
 	total_pages_processed = 0;
-	pages_remaining = *number_of_pages;
 
-	while (pages_remaining > 0) {
-		for (i = 0 ;
-			 ((i < max_pages_per_request) &&
-				((total_pages_processed + i) < *number_of_pages)) ;
-			 i++)
+	while (total_pages_processed < *number_of_pages) {
+		for (i = 0; ((i < max_pages_per_request) &&
+			     ((total_pages_processed + i) < *number_of_pages)); i++)
 			hvin->gpa_page_list[i] = gpa_page_list[total_pages_processed + i];
 
-		/* Disable interrupts */
-		local_irq_save(flags);
-
 		/* Perform the hypercall */
-		status = vsm_hv_hypercall(HVCALL_MODIFY_VTL_PROTECTION_MASK, hvin, NULL, i, &pages_processed);
-
-		/* Enable interrupts */
-		local_irq_restore(flags);
+		status = hv_do_rep_hypercall(HVCALL_MODIFY_VTL_PROTECTION_MASK, i, 0, hvin, NULL);
 
 		/*
 		 * Update page accounting for the next iteration, if any
-		 *
 		 * N.B.: pages_processed is correct even if Hyper-V returned an error.
 		 */
-		pages_remaining -= pages_processed;
+		pages_processed = hv_repcomp(status);
 		total_pages_processed += pages_processed;
 
-		pr_info("HVCALL_MODIFY_VTL_PROTECTION_MASK status%d", status);
-
 		/* See how things went */
-		if (status != HV_STATUS_SUCCESS)
+		if (!hv_result_success(status))
 			break;
 	}
 
 	/* Pass results out (valid on error) */
 	*number_of_pages = total_pages_processed;
 
+	/* Enable interrupts */
+	local_irq_restore(flags);
+
 	/* Done */
-	return status == HV_STATUS_SUCCESS ? 0 : -EPERM;
+	return hv_result(status);
 }
 
 static int vsm_restrict_memory(u64 start, size_t size, u32 permissions)
 {
-	int res=-EPERM;
-	size_t i;
-	size_t page_count;
+	int res = -EPERM;
+	size_t page_count, i;
 	u64 *pfns;
 
 	// Assert that the region to protect is page-aligned 
@@ -167,7 +126,7 @@ static void mshv_vsm_handle_entry(struct mshv_vtl_call_params *_vtl_params)
 	switch (_vtl_params->_a0) {
 		case VSM_VTL_CALL_FUNC_ID_PROTECT_MEMORY:
 			pr_info("%s : VSM_PROTECT_MEMORY\n", __func__);
-			pr_info("%s : a0:%lX a1:%lX, a2:%lX,a3:%lX\n", __func__,
+			pr_info("%s : a0:%llx a1:%llx, a2:%llx, a3:%llx\n", __func__,
 				_vtl_params->_a0, _vtl_params->_a1, _vtl_params->_a2,
 				_vtl_params->_a3);
 
@@ -210,9 +169,7 @@ static void mshv_vsm_vtl_return()
 	unsigned long irq_flags;
 	struct hv_vp_assist_page *hvp;
 
-	pr_info("%s : a0:%llx a1:%llx, a2:%llx,a3:%llx\n", __func__,
-			vtl_params._a0, vtl_params._a1, vtl_params._a2,vtl_params._a3);
-	/* Ordering is important. Suspedn tick before disabling interrupts */
+	/* Ordering is important. Suspend tick before disabling interrupts */
 	tick_suspend_local();
 	local_irq_save(irq_flags);
 	kernel_fpu_begin_mask(0);
@@ -270,46 +227,30 @@ static void mshv_vsm_vtl_return()
 	mshv_vsm_handle_entry(&vtl_params);
 }
 
-static int mshv_vtl1_configure_vsm_partition(void)
+static int mshv_vsm_configure_partition(void)
 {
+	union hv_vsm_partition_config config;
+	struct hv_register_assoc reg_assoc;
 	union hv_input_vtl input_vtl;
-	u64 flags;
-	enum hv_status status;
-	struct hv_input_set_vp_registers *hvin;
-	union hv_register_value register_value;
-	union hv_vsm_partition_config *vsm_partition_config;
-	u32 elements_processed;
 
-	/* Enable and set default VTL protections */
-	vsm_partition_config = (union hv_vsm_partition_config *)
-		&register_value;
-	vsm_partition_config->as_u64 = 0;
-	vsm_partition_config->enable_vtl_protection = 1;
-	vsm_partition_config->default_vtl_protection_mask =
-		HV_MAP_GPA_ACCESS_ALL;
+	config.as_u64 = 0;
+	config.default_vtl_protection_mask = HV_MAP_GPA_PERMISSIONS_MASK;
+	config.enable_vtl_protection = 1;
+//	config.zero_memory_on_reset = 1;
+//	config.intercept_vp_startup = 1;
+//	config.intercept_cpuid_unimplemented = 1;
 
-	/* Acquire the input page */
-	hvin = hv_acquire_hypercall_input_page();
+/*	if (mshv_vsm_capabilities.intercept_page_available) {
+		pr_debug("%s: using intercept page", __func__);
+		config.intercept_page = 1;
+	}
+*/
+	reg_assoc.name = HV_REGISTER_VSM_PARTITION_CONFIG;
+	reg_assoc.value.reg64 = config.as_u64;
+	input_vtl.as_uint8 = 0;
 
-	/* Fill in the hypercall parameters */
-	hvin->partition_id = HV_PARTITION_ID_SELF;
-	hvin->vp_index = HV_VP_INDEX_SELF;
-	hvin->input_vtl.as_uint8 = 0;
-	hvin->reserved8_z = 0;
-	hvin->reserved16_z = 0;
-	hvin->elements[0].name = HV_REGISTER_VSM_PARTITION_CONFIG;
-	hvin->elements[0].value = register_value;
-
-	/* Disable interrupts */
-	local_irq_save(flags);
-
-	/* Perform the hypercall */
-	status = vsm_hv_hypercall(HVCALL_SET_VP_REGISTERS, hvin, NULL, 1,
-		&elements_processed);
-
-	/* Enable interrupts */
-	local_irq_restore(flags);
-	return status;
+	return hv_call_set_vp_registers(HV_VP_INDEX_SELF, HV_PARTITION_ID_SELF,
+					1, input_vtl, &reg_assoc);
 }
 
 static long mshv_vsm_vtl_ioctl(struct file *filp, unsigned int ioctl, unsigned long arg)
@@ -350,7 +291,7 @@ static int __init mshv_vtl1_init(void)
 		pr_err("VSM: Could not register mshv_vsm_vtl_ioctl\n");
 	}
 
-	if (mshv_vtl1_configure_vsm_partition()) {
+	if (mshv_vsm_configure_partition()) {
 		pr_emerg("%s: VSM configuration failed !!\n", __func__);
 		return -EPERM;
 	}
