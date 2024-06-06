98587868283793047a188d9d8692d7d8bfbee8d0
Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
2024-06-06 09:35:25 -0400

    heki: Lock guest control registers at the end of guest kernel
    The hypervisor needs to provide some functions to support Heki. These
    form the Heki-Hypervisor API.
    
    Define a heki_hypervisor structure to house the API functions. A
    hypervisor that supports Heki must instantiate a heki_hypervisor
    structure and pass it to the Heki common code. This allows the common
    code to access these functions in a hypervisor-agnostic way.
    
    The first function that is implemented is lock_crs() (lock control
    registers). That is, certain flags in the control registers are pinned
    so that they can never be changed for the lifetime of the guest.
    
    Implement Heki support in the guest:
    
    - Each supported hypervisor in x86 implements a set of functions for the
      guest kernel. Add an init_heki() function to that set.  This function
      initializes Heki-related stuff. Call init_heki() for the detected
      hypervisor in init_hypervisor_platform().
    
    - Implement init_heki() for the guest.
    
    - Pass the heki_hypervisor structure to Heki common code in init_heki().
    
    Implement a heki_late_init() function and call it at the end of kernel
    init. This function calls lock_crs(). In other words, control registers
    of a guest are locked down at the end of guest kernel init.
    
    Signed-off-by: Mickaël Salaün <mic@digikod.net>
    Signed-off-by: Madhavan T. Venkataraman <madvenka@linux.microsoft.com>

--- init/main.c
+++ init/main.c
@@ -1451,6 +1451,7 @@ static int __ref kernel_init(void *unused)
 	exit_boot_config();
 	free_initmem();
 	mark_readonly();
+	heki_late_init();
 
 	/*
 	 * Kernel mappings are now finalized - update the userspace page-table
