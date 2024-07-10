01e28e61ecbf24b64b2bb3f24662cd51e08c50f1
Madhavan T. Venkataraman <madvenka@microsoft.com>
2024-07-10 12:31:48 -0500

    Receive a VTL0 module and validate its contents
    Implement a hypercall to receive a module blob from VTL0 and
    check the module's signature against the trusted keyring in VTL1.
    
    After the signature is checked successfully, execute the steps to layout
    and allocate the module sections in VTL1 just like in VTL0.
    
    Compare the module sections constructed in VTL1 with the ones passed
    from VTL0. If they match, then the guest module contents are validated.
    
    Return an authentication token to VTL0 so VTL0 can refer to VTL1's copy
    of the module in the future.
    
    Signed-off-by: Madhavan T. Venkataraman <madvenka@linux.microsoft.com>

--- include/linux/heki.h
+++ include/linux/heki.h
@@ -48,6 +48,15 @@ struct heki_page {
 /*
  * The ranges contain VTL0 pages. VTL0 pages are mapped into VTL1 address space
  * so VTL1 can access VTL0 memory at va.
+ *
+ * Each module section (text, data, etc) is represented by a heki_mem. Module
+ * sections are reconstructed in VTL1 and compared with the corresponding
+ * VTL0 sections. Reconstruction involves module symbol resolution and module
+ * relocation. These steps involve symbol addresses. To make the reconstruction
+ * simpler, we map the VTL1 module sections at the same virtual addresses as
+ * their corresponding sections in VTL0. We call this identity mapping. This
+ * keeps the addresses the same in VTL0 and VTL1. A VTL1 section is accessed
+ * at ranges->va since that is the starting va for the section.
  */
 struct heki_mem {
 	void			*va;
@@ -55,11 +64,31 @@ struct heki_mem {
 	long			offset;
 	struct heki_range	*ranges;
 	unsigned long		nranges;
+	struct page		**pages;
+	bool			retain;
 };
 
 enum heki_kdata_type {
 	HEKI_MODULE_CERTS,
 	HEKI_KDATA_MAX,
 };
+
+/*
+ * Attribute value for module ELF that does not conflict with any of the
+ * values in enum mod_mem_type.
+ */
+#define MOD_ELF		MOD_MEM_NUM_TYPES
+
+/* This is created for each guest module in the host. */
+struct heki_mod {
+	struct list_head node;
+	struct heki_range *ranges;
+	char name[MODULE_NAME_LEN];
+	long token;
+	struct heki_mem mem[MOD_ELF + 1];
+	struct module *mod;
+};
+
+#define HEKI_MODULE_RESERVE_SIZE	0x40000000UL
 
 #endif /* __HEKI_H__ */
